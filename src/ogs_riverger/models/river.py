673cada00472e9cb3144de794a4d5e389ceabe99
Stefano Piani <spiani@ogs.it>
2026-07-28 11:46:24 +0200

    Fixed indentation using pre-commit

--- src/ogs_riverger/models/river.py
+++ src/ogs_riverger/models/river.py
@@ -637,7 +637,9 @@ class FromDatasetRiverComponent(RiverComponent):
             time_diff = np.abs(
                 self._time_array[values_indices] - requested_times
             )
-            max_error = np.timedelta64(int(self._max_error.total_seconds() * 1000), "ms")
+            max_error = np.timedelta64(
+                int(self._max_error.total_seconds() * 1000), "ms"
+            )
             if np.any(time_diff > max_error):
                 t_index = np.where(time_diff > max_error)[0][0]
                 t = time_list[t_index]
